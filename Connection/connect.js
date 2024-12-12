56cc0a405f4f7834142e533fb6506b2cde9376a9
Abhishek <abhisheksangule6@gmail.com>
2024-12-12 23:22:27 +0530

    change the mongo connection

--- Connection/connect.js
+++ Connection/connect.js
@@ -1,8 +1,11 @@
 const mongoose = require("mongoose");
 
 async function connectToMongo(url) {
-    return mongoose.connect(url,{ useNewUrlParser: true, useUnifiedTopology: true,ssl: true,sslValidate: false, // Disable validation if needed (use with caution)
-        tlsCAFile: '/path/to/ca.pem'}); 
+    return mongoose.connect('mongodb+srv://abhisheksangule6:3v35GLNLtpovw4DV@urlshortener.5uo5s.mongodb.net/short_url?retryWrites=true&w=majority&appName=URLShortener', {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        ssl: true,
+      })
 }
 
 module.exports = {
